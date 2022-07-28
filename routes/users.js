930699aaae32a68309d7ae005a7c9dda411deed2
Anastasia Semenova <n4stenkas91@yandex.ru>
2022-07-28 10:03:02 +0300

    доработать маршруты

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 const routerUsers = require('express').Router();
 const { getMe, updateMe } = require('../controllers/users');
+const { validateUserUpdate } = require('../middlewares/validation');
 
 routerUsers.get('/me', getMe);
 
-routerUsers.patch('/me', updateMe);
+routerUsers.patch('/me', validateUserUpdate, updateMe);
 
 module.exports = routerUsers;
